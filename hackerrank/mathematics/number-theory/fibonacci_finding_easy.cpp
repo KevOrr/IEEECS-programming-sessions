2a7cddacd6d648926fa5d2304d1f15857ec17314
Kevin Orr <kevinorr54@gmail.com>
2017-02-09 03:56:47 -0500

    Getting there

--- hackerrank/mathematics/number-theory/fibonacci_finding_easy.cpp
+++ hackerrank/mathematics/number-theory/fibonacci_finding_easy.cpp
@@ -2,8 +2,11 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <string.h>
+#include <vector>
 
 const uint_fast64_t mod = 1000000007; // 1e9 + 7
+std::vector<uint_fast64_t> fibs;
+std::vector<bool> already_calculated;
 
 void matrix_multiply(uint_fast64_t A[2][2], uint_fast64_t B[2][2]) {
     uint_fast64_t temp[2][2];
@@ -43,6 +46,34 @@ void old_calculate_matrix(uint_fast64_t original_matrix[2][2], uint_fast64_t n) 
     }
 }
 
+uint_fast64_t fib_n(uint_fast64_t n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    if (fibs.size() < n+1) {
+        fibs.resize(n+1, -1);
+        already_calculated.resize(n+1, false);
+    } else if (already_calculated[n])
+        return fibs[n];
+
+    if (n & 1) {
+        uint_fast64_t half_n = (n+1)/2;
+        uint_fast64_t F_half_n = fib_n(half_n);
+        uint_fast64_t F_half_n_m1 = fib_n(half_n-1);
+
+        already_calculated[n] = true;
+        return fibs[n] = F_half_n*F_half_n + F_half_n_m1*F_half_n_m1;
+    } else {
+        uint_fast64_t half_n = n/2;
+        uint_fast64_t F_half_n = fib_n(half_n);
+
+        already_calculated[n] = true;
+        return fibs[n] = (2*fib_n(half_n-1) + F_half_n)*F_half_n;
+    }
+}
+
 uint_fast64_t recurrence_n(uint_fast64_t a, uint_fast64_t b, uint_fast64_t n) {
     // this is redundant since you have to check in calculate_matrix() anyway, but whatever
     if (n == 0)
@@ -50,9 +81,7 @@ uint_fast64_t recurrence_n(uint_fast64_t a, uint_fast64_t b, uint_fast64_t n) {
     if (n == 1)
         return b;
 
-    uint_fast64_t matrix[2][2] = {{1, 1}, {1, 0}};
-    calculate_matrix(matrix, n-1);
-    return matrix[0][0]*b + matrix[0][1]*a;
+    return fib_n(n)*b + fib_n(n-1)*a;
 }
 
 int main() {
