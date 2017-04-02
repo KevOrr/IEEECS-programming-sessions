18493d2971a8ef6871a5ffa5562e41b62fdce85c
Kevin Orr <kevinorr54@gmail.com>
2017-04-01 23:32:48 -0400

    More work on hackerrank/maze_escape

--- hackerrank/artificial_inteligence/bot_building/maze_escape.py
+++ hackerrank/artificial_inteligence/bot_building/maze_escape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import itertools
 
 class Board():
     EXIT = 'e'
@@ -14,7 +15,8 @@ class Board():
         self._offset = (30, 30) # the actual index of (0, 0) in self._internal
         self._position = (0, 0) # the current location on the board (not in self._internal)
         self._orientation = 'u'
-        self._bounds = (-30, -30, 30, 30) # all inclusive
+        self._bounds = (-30, 30, -30, 30) # all inclusive
+        self._update([tuple(sys.stdin.readline().strip('\n').lower()) for i in range(3)])
 
     @property
     def position(self):
@@ -28,22 +30,21 @@ class Board():
     def bounds(self):
         return self._bounds
 
-    # maxx and maxy are exclusive
-    def _resize_board(self, minx=None, maxx=none, miny=None, maxy=None):
-        if sum(int(bool(i)) for i in (minx, maxx, miny, maxy)) != 1:
-            e = ValueError('Exactly one of minx, maxx, miny, maxy should be True')
-            raise e
+    def __getitem__(self, sel):
+        return self._internal[sel[1]+self._offset[1]][sel[0]+self._offset[0]]
 
-        if minx is not None:
-            self._internal = [row[minx:] for row in self._internal]
-        elif maxx is not None:
-            self._internal = [row[:maxx] for row in self._internal]
-        elif miny is not None:
-            self._internal = [row[:] for row in self._internal[miny:]]
-        else:
-            self._internal = [row[:] for row in self._internal[:maxy]]
+    def __setitem__(self, sel, obj):
+        old = self[sel]
+        self._internal[sel[1]+self._offset[1]][sel[0]+self._offset[0]] = obj
+        return old
 
-    def _update(self, )
+    def __str__(self):
+        s = ''
+        for y in range(self._bounds[2], self._bounds[3]):
+            for x in range(self._bounds[0], self._bounds[1]):
+                s += self[x, y]
+            s += '\n'
+        return s[:-1]
 
     def move(self, direction):
         direction = direction.lower()
@@ -71,15 +72,38 @@ class Board():
             e = ValueError('Can\'t move into a blocked space')
             raise e
         elif self[targetx, targety] == Board.EMPTY:
-            print {'u': 'UP', 'd': 'DOWN', 'l': 'LEFT', 'r': 'RIGHT'}[direction]
-            self._update([list(row.strip()) for row in (input(), input(), input())], )
+            print({'u': 'UP', 'd': 'DOWN', 'l': 'LEFT', 'r': 'RIGHT'})[direction]
+            self._orientation = direction
             self._position = (targetx, targety)
+            self._update([tuple(sys.stdin.readline().strip('\n').lower()) for i in range(3)])
+            return self._position
+        elif self[targetx, targety] == Board.EXIT:
+            pass
         else:
-            # Should never reach a tile that is EXIT (program should just exit)
-            # or UNEXPLORED (neighbor tiles are always visible, so a bot should never)
-            # land on a tile before it is seen
-            e = RuntimeError('Erroneously reached an EXIT or UNEXPLORED tile')
+            # Should never reach a tile that is UNEXPLORED (neighbor tiles are
+            # always visible, so a bot should never land on a tile before it is seen)
+            e = RuntimeError('Erroneously reached an UNEXPLORED tile')
             raise e
 
-    def __getitem__(self, sel):
-        return self._internal[sel[0]+self._offset[0]][sel[1]+self._offset[1]]
+    def _update(self, vision):
+        if self._orientation == 'd':
+            for row in vision:
+                row.reverse()
+            vision.reverse()
+        elif self._orientation == 'l':
+            vision = list(zip(*vision))
+            vision.reverse()
+        elif self._orientation == 'r':
+            vision.reverse()
+            vision = list(zip(*vision))
+
+        for x,y in set(itertools.product(range(-1,2), repeat=2)) - set([(0, 0)]):
+            board_position = (self.position[0] + x, self.position[0] + y)
+            if self[board_position] == Board.UNEXPLORED:
+                self[board_position] = vision[y+1][x+1]
+            elif self[board_position] != vision[y+1][x+1]:
+                e = RuntimeError('Robot\'s vision inconsistent with board')
+                raise e
+
+if __name__ == '__main__':
+    b = Board()
